2d8570caa6e45a8bdfd6dcb3d9d7d8c4abe63152
kempesaguiar <kempesaguiar@gmail.com>
2023-04-24 07:52:13 -0300

    estrutura inicial

---
+++ src/main/java/br/com/kca/api/models/Cliente.java
@@ -0,0 +1,67 @@
+package br.com.kca.api.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 160)
+	private String nome;
+	
+	@Column(nullable = false, length = 80)
+	private String email;
+	
+	@Column(nullable = true, length = 60)
+	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	
+
+}
