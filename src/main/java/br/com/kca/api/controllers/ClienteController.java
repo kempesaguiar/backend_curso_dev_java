b625a93256692f4dba13641b5b723dbeae02297b
kempesaguiar <kempesaguiar@gmail.com>
2023-07-19 19:41:51 -0300

    aula 15

--- src/main/java/br/com/kca/api/controllers/ClienteController.java
+++ src/main/java/br/com/kca/api/controllers/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import br.com.kca.api.dto.LoginDTO;
 import br.com.kca.api.exception.ClienteNotFoundException;
 import br.com.kca.api.services.ClienteService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
